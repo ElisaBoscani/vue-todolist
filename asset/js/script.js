256ca8401f435f74bbbc4945db78580b5213f368
Elisa Boscani <elisabosc90@gmail.com>
2023-09-13 17:44:27 +0200

    to do list completed

--- asset/js/script.js
+++ asset/js/script.js
@@ -3,16 +3,27 @@ createApp({
   data() {
     return {
       newText: "",
-      textList: [],
+      textList: [
+        { list: "fare la spesa", done: false },
+        { list: "fare la lavatrice", done: false },
+        { list: "visita dal dentista alle 18", done: false },
+        { list: "ritirare un pacco", done: false },
+      ],
     };
   },
   methods: {
     addText() {
       if (this.newText != "") {
-        this.textList.push({ done: true, list: this.newText });
+        this.textList.unshift({ done: false, list: this.newText });
         console.log(this.newText);
       }
       this.newText = "";
+    },
+    removeText(index) {
+      this.textList.splice(index, 1);
+    },
+    checkText(text) {
+      text.done = !text.done;
     },
   },
 }).mount("#app");
